6152f265c6b756503f3bd436699116b379fb35d3
SenKanna43 <senthamaraikannanp@gmail.com>
2024-01-23 18:13:00 +0530

    state management

---
+++ ngrx-counter/src/app/counter/counter.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppState } from '../states/app.state';
+import { Store } from '@ngrx/store';
+import { selectCount } from '../states/counter/counter.selector';
+import { decrement, increment, reset } from '../states/counter/counter.action';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+
+  count$ !: Observable<number>
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select(selectCount)
+  }
+
+  increment() {
+    this.store.dispatch(increment())
+  }
+
+  decrement() {
+    this.store.dispatch(decrement())
+  }
+
+  reset() {
+    this.store.dispatch(reset())
+  }
+}
